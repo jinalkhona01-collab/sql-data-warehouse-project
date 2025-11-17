27611099c9793e886ba61d93b871e35456d4e539
Jinal Khona <jinal.khona01@gmail.com>
2025-11-17 22:11:31 +0100

    Create DataWarehouse database and schemas
    This script creates a new database named 'DataWarehouse' and sets up three schemas: 'bronze', 'silver', and 'gold'.

---
+++ scripts/init_database.sql
@@ -0,0 +1,24 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse'. 
+    Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE "DataWarehouse"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LOCALE_PROVIDER = 'libc'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
+
