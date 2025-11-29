3a81d158325a78100f34d01db276273466ebecab
Jinal Khona <jinal.khona01@gmail.com>
2025-11-29 14:08:26 +0100

    Add database exploration SQL script
    This script explores the database structure, listing tables and their schemas, as well as inspecting columns and metadata for the 'dim_customers' table.

---
+++ data-analytics/eda_scripts/01_database_exploration.sql
@@ -0,0 +1,24 @@
+/*
+===============================================================================
+Database Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of the database, including the list of tables and their schemas.
+    - To inspect the columns and metadata for specific tables.
+
+Table Used:
+    - INFORMATION_SCHEMA.TABLES
+    - INFORMATION_SCHEMA.COLUMNS
+===============================================================================
+*/
+
+--Explore All objects in the Database
+SELECT * 
+FROM INFORMATION_SCHEMA.TABLES;
+
+
+--Explore All Columns in the Database
+SELECT * 
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_customers'
+ORDER BY ordinal_position ;
